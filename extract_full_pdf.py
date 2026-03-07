4626dd42ab1cac49d5c44f02403baec5c0086fe0
R.Y.S <redjahyousra6@gmail.com>
2026-03-07 09:18:42 +0100

    Update extract_full_pdf.py

--- extract_full_pdf.py
+++ extract_full_pdf.py
@@ -1,7 +1,7 @@
 import pdfplumber
 
-with pdfplumber.open(r'c:\Users\rog wephyrus\Desktop\Projet TP Compile WITH IA Agents\Projet M1_compil 25-26.pdf') as pdf:
-    with open(r'c:\Users\rog wephyrus\Desktop\Projet TP Compile WITH IA Agents\Context\all_text.txt', 'w', encoding='utf-8') as f:
+with pdfplumber.open(r'Projet TP Compile WITH IA Agents\Projet M1_compil 25-26.pdf') as pdf:
+    with open(r'TP Compile WITH IA Agents\Context\all_text.txt', 'w', encoding='utf-8') as f:
         for i, page in enumerate(pdf.pages):
             f.write(f"--- PAGE {i+1} ---\n")
             f.write(page.extract_text() or "")
